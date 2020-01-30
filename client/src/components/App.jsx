2ada4d00534a852fa4a07e66aa18048be59a4bf0
Victor Tu <46986265+dashprotocol@users.noreply.github.com>
2020-01-29 20:40:24 -0800

    Store products data into state

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,6 +1,33 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      products: []
+    };
+    this.getProducts = this.getProducts.bind(this);
+  }
+
+  componentDidMount() {
+    this.getProducts();
+  }
+
+  getProducts() {
+    axios.get('/products')
+      .then((response) => {
+        this.setState({
+          products: response.data
+        });
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+      .finally(function () {
+      })
+  }
+
   render() {
     return (
       <div>
