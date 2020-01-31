d5782096022ffa8fb4e29ca059a2a3f36201a909
Victor Tu <46986265+dashprotocol@users.noreply.github.com>
2020-01-31 11:06:52 -0800

    Initialize state with a single product with dummy data for easier development of component

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/no-unused-state */
+/* eslint-disable no-console */
 import React, { Component } from 'react';
 import axios from 'axios';
 
@@ -5,7 +7,26 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      products: []
+      // eslint-disable-next-line react/no-unused-state
+      products: [],
+      product: {
+        __v: 0,
+        _id: 4,
+        course: {
+          name: 'Awesome Concrete Shirt',
+          image: 'http://lorempixel.com/640/480/cats',
+          video: 'http://lorempixel.com/640/480/sports',
+          category: 'sysadmin',
+          lengthInHours: 449,
+          articleCount: 89,
+          resourceCount: 59,
+        },
+        lastUpdateDate: '2019-05-25T06:38:38.037Z',
+        originalPrice: 649,
+        salesPrice: 580,
+        totalNumOfPurchases: 114141,
+        averageRating: 4,
+      },
     };
     this.getProducts = this.getProducts.bind(this);
   }
@@ -18,14 +39,15 @@ class App extends Component {
     axios.get('/products')
       .then((response) => {
         this.setState({
-          products: response.data
+          // eslint-disable-next-line react/no-unused-state
+          products: response.data,
         });
       })
-      .catch(function (error) {
+      .catch((error) => {
         console.log(error);
       })
-      .finally(function () {
-      })
+      .finally(() => {
+      });
   }
 
   render() {
