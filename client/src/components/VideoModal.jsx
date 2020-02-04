4e75f12c0cb9c48acfbe8ffa9b5e6ac3be8f7ea8
Victor Tu <46986265+dashprotocol@users.noreply.github.com>
2020-02-03 23:09:12 -0800

    Display Free Sample Videos text in Video modal

--- client/src/components/VideoModal.jsx
+++ client/src/components/VideoModal.jsx
@@ -4,6 +4,7 @@ import Background from '../elements/VideoModal/Background';
 import VideoModalWrapper from '../elements/VideoModal/VideoModalWrapper';
 import CoursePreviewTitle from '../elements/VideoModal/CoursePreviewTitle';
 import VideoPlayer from '../elements/VideoModal/VideoPlayer';
+import SampleVideoText from '../elements/VideoModal/SampleVideoText';
 
 const videoModal = document.getElementById('video-modal');
 
@@ -16,6 +17,9 @@ class VideoModal extends React.Component {
             Course Preview: Learn Python in 24 Days
           </CoursePreviewTitle>
           <VideoPlayer src="https://www.youtube.com/embed/_uQrJ0TkZlc" />
+          <SampleVideoText>
+            Free Sample Videos:
+          </SampleVideoText>
         </VideoModalWrapper>
       </Background>,
       videoModal,
---
+++ client/src/elements/VideoModal/SampleVideoText.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const SampleVideoText = styled.div`
+  color: #dedfe0;
+  font-size: 15px;
+  padding: 16px;
+  display: inline-block;
+  box-sizing: border-box;
+  font-family: open sans,helvetica neue,Helvetica,Arial,sans-serif;
+`;
+
+export default SampleVideoText;
