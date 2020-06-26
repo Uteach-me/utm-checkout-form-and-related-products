981d0ba339003efa500f2c8350a26e6de34bdebc
Victor Tu <46986265+dashprotocol@users.noreply.github.com>
2020-06-26 15:25:54 -0700

    Add new server route to handle gzipped files

--- server/index.js
+++ server/index.js
@@ -8,13 +8,22 @@ const axios = require('axios');
 const controller = require('./controller.js');
 const cors = require('cors');
 
+
 // Instantiate the express server
 const app = express();
 // Set a constant for the port that our express server will listen on
 const PORT = 3007;
 
+
 app.use(cors());
 app.use(morgan('dev'));
+
+app.get('*.js', (req, res, next) => {
+  req.url = req.url + '.gz';
+  res.set('Content-Encoding', 'gzip');
+  next();
+});
+
 // Serve static files. Any requests for specific files will be served if they exist
 // in the provided folder
 app.use(express.static(path.join(__dirname, '../client/dist')));
